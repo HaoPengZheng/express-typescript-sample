7f6d0aa7de2b3066b4216feea71332e10af26ddf
zhp <354416479@qq.com>
2019-06-02 15:05:32 +0800

    add cros

--- src/app.ts
+++ src/app.ts
@@ -30,6 +30,17 @@ import * as passportConfig from "./config/passport";
 // Create Express server
 const app = express();
 
+// 设置允许跨域访问该服务.
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  // Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
+
 // Connect to MongoDB
 const mongoUrl = MONGODB_URI;
 (<any>mongoose).Promise = bluebird;
